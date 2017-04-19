44ea3662c1799cc448d02159b9c0437880e6baf4
pauliewax <pauliewax@gmail.com>
2017-04-19 17:08:20 -0400

    Add logout button to navBar for testing

--- frontend/components/nav_bar.jsx
+++ frontend/components/nav_bar.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { hashHistory } from 'react-router';
 import { connect } from 'react-redux';
+import { logout } from '../actions/session_actions';
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
+    this.logout = this.logout.bind(this);
+  }
+
+  logout() {
+    this.props.logout();
   }
 
   render() {
     let navButton;
+
+    if (this.props.loggedIn) {
+      navButton = <button className="button" onClick={this.logout}>Log Out</button>;
+    } else {
+      navButton = <button className="button" onClick={()=>hashHistory.push("/login")}>Sign In</button>;
+    }
+
     return(
       <div>
         <nav className="navBar">
           <img className="navLogo" src="https://vignette4.wikia.nocookie.net/smurfs/images/a/a1/Netflix-logo.png/revision/latest?cb=20150508223333" />
-          <button className="button" onClick={()=>hashHistory.push("/login")}>Sign In</button>
+          { navButton }
         </nav>
       </div>
     );
@@ -21,4 +34,18 @@ class NavBar extends React.Component {
 
 }
 
-export default NavBar;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    loggedIn: state.session.currentUser ? true : false,
+    // formType: ownProps.location.pathname
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    logout: () => (dispatch(logout()))
+  };
+
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(NavBar);
