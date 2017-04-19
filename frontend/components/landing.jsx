74660ce9ec034f3f2256fc5b4ca600feb8283122
pauliewax <pauliewax@gmail.com>
2017-04-19 18:26:05 -0400

    Test css

--- frontend/components/landing.jsx
+++ frontend/components/landing.jsx
@@ -10,7 +10,7 @@ const Landing = () => (
       <button className="button" onClick={()=>hashHistory.push("/login")}>Join Free For a Month</button>
     </section>
 
-    <img className="daImg" src="https://assets.nflxext.com/ffe/siteui/vlv2/1e1fc120-ce81-48e2-8229-514cd3e414e5/a8e0c04a-c030-420f-8c4f-d44910d28bbb/US-en-20170417-popsignuptwoweeks-perspective_alpha_website_large.jpg" />
+    <img src="https://assets.nflxext.com/ffe/siteui/vlv2/1e1fc120-ce81-48e2-8229-514cd3e414e5/a8e0c04a-c030-420f-8c4f-d44910d28bbb/US-en-20170417-popsignuptwoweeks-perspective_alpha_website_large.jpg" />
 
   </div>
 );
