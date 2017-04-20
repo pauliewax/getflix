b21cdaf23f19fc6f153b4de89833769c214a8966
pauliewax <pauliewax@gmail.com>
2017-04-20 00:00:17 -0400

    Add reminder for removing test variables on window

--- frontend/getflix.jsx
+++ frontend/getflix.jsx
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       store = configureStore();
     }
+    // REMOVE WINDOW STORE IN PRODUCTION
     window.store = store;
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={ store } />, root);
