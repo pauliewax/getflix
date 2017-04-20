f8a4d9eab4bab6fa1ff25425b035dbfa3ed396f3
pauliewax <pauliewax@gmail.com>
2017-04-20 19:35:37 -0400

    Fix polymorphic assoc in genre model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,6 @@ class Genre < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   has_many :categorizations
-  has_many :medias, :through => :categorization
+  has_many :videos, :through => :categorizations, :source => :media, :source_type => 'Video'
+  has_many :series, :through => :categorizations, :source => :media, :source_type => 'Series'
 end
