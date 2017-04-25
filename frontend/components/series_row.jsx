dfb7402b6ec39171bea456cdf30206e99b63d047
pauliewax <pauliewax@gmail.com>
2017-04-25 12:13:17 -0400

    Rework slider CSS

--- frontend/components/series_row.jsx
+++ frontend/components/series_row.jsx
@@ -8,39 +8,17 @@ class SeriesRow extends React.Component {
       super(props);
 
       this.state = {selectedSerie: ''};
-      // this.handleClick = this.handleClick.bind(this);
+      this.handleClick = this.handleClick.bind(this);
   }
 
 
-  // handleClick(event) {
-  //   this.setState({selectedSerie: event.currentTarget.className}, this.closeAllViewers);
-  // }
-  //
-  // // componentWillReceiveProps(newProps){
-  // //   let thisSeriesIds = this.props.row.map(serie=>serie.id);
-  // //
-  // //   if (newProps.seriesDetail !== this.props.seriesDetail) {
-  // //     if (thisSeriesIds.includes(newProps.seriesDetail.id))
-  // //     this.openThisViewer();
-  // //   }
-  // // }
-  //
-  // closeAllViewers() {
-  //   let viewers = document.getElementsByClassName('viewer');
-  //   for (var i = 0; i < viewers.length; i++) {
-  //     viewers[i].classList.remove('viewerSlideOut');
-  //   }
-  // }
-  //
-  // openThisViewer() {
-  //   let myViewer = document.getElementsByClassName(`viewer-${this.props.rowId}`)[0];
-  //   myViewer.classList.add('viewerSlideOut');
-  // }
-
+  handleClick(event) {
+    this.setState({selectedSerie: event.currentTarget.className});
+  }
 
   render()  {
 
-    let series = this.props.row.map(serie=><li className={serie.id} key={serie.id} ><Series  serie={serie} /></li>);
+    let series = this.props.row.map(serie=><li onClick={this.handleClick} className={`${serie.id} serieWrapper`} key={serie.id} ><Series  serie={serie} /></li>);
 
     let serieId = series[0].key;
     if (this.state.selectedSerie !== '') {
@@ -50,7 +28,7 @@ class SeriesRow extends React.Component {
     return(
       <div className="seriesRow">
         { series }
-        <aside className="clearFlex"/>
+
         <div className={`viewer viewer-${this.props.rowId}`}><SeriesViewer thisViewerId={`viewer-${this.props.rowId}`} serieId={serieId}/></div>
       </div>
     );
