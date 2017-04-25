1d9ef4a35414a0bc93e29d163475ef20e35e5428
pauliewax <pauliewax@gmail.com>
2017-04-25 14:39:46 -0400

    Mostly styled seriesViewer

--- app/views/api/series/show.json.jbuilder
+++ app/views/api/series/show.json.jbuilder
@@ -1,6 +1,9 @@
 json.id @serie.id
 json.title @serie.title
+json.description @serie.description
 json.url @serie.thumbnail_url
+json.year @serie.year
+json.rating @serie.mpaa_rating
 
 json.episodes @serie.videos do |episode|
   json.id episode.id
--- frontend/components/series_viewer.jsx
+++ frontend/components/series_viewer.jsx
@@ -17,14 +17,36 @@ class SeriesViewer extends React.Component {
   render() {
 
     let name;
+    let body;
+    let rating;
+    let year;
+    let img;
 
     if (this.props.seriesDetail) {
       name = this.props.seriesDetail.title;
+      body = this.props.seriesDetail.description;
+      rating = this.props.seriesDetail.rating;
+      year = this.props.seriesDetail.year;
+      img = this.props.seriesDetail.url;
     }
+    // debugger
 
     return(
       <div className="seriesViewer">
-        <p>{ name }</p>
+        <summary>
+          <div className="viewTitle">{ name }</div>
+          <section>
+            <aside className="viewDetails">{year}</aside>
+            <aside>{rating}</aside>
+          </section>
+          <main className="viewBody">
+            { body }
+          </main>
+        </summary>
+        <div className="viewerBg">
+          <div className="viewerBgFade"/>
+          <img src={img}></img>
+        </div>
       </div>
     );
   }
