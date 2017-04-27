c4b0300eeb9c0b779a52e6a331144e3899a41aa5
pauliewax <pauliewax@gmail.com>
2017-04-27 15:08:55 -0400

    Working play slider

--- frontend/components/player_controls.jsx
+++ frontend/components/player_controls.jsx
@@ -5,22 +5,50 @@ class PlayerControls extends React.Component {
   constructor(props) {
     super(props);
     this.pressPlay = this.pressPlay.bind(this);
+    this.handleSlide = this.handleSlide.bind(this);
+    this.state = {seekerVal: 0};
+  }
+
+  componentDidMount() {
+    let video = document.getElementById("videoPlayer");
+    video.addEventListener("timeupdate", function() {
+      let value = ( 100 / video.duration ) * video.currentTime;
+      this.setState({seekerVal: value});
+    }.bind(this));
   }
 
   pressPlay() {
     let video = document.getElementById("videoPlayer");
+    let play = document.getElementById('videoPlay');
+    let pause = document.getElementById('videoPause');
+
     if (video.paused === true) {
       video.play();
+      play.classList.add('hidePlay');
+      pause.classList.remove('hidePlay');
     } else {
       video.pause();
+      play.classList.remove('hidePlay');
+      pause.classList.add('hidePlay');
     }
+  }
+
+  handleSlide(e) {
+    this.setState({seekerVal: e.target.value});
+    let video = document.getElementById("videoPlayer");
+    let time = video.duration * (e.target.value / 100);
+    video.currentTime = time;
   }
 
   render() {
     return(
       <div className="playerControls">
         <div className="playBar">
-          <i onClick={this.pressPlay} id="videoPlay" className="fa fa-play"></i>
+          <div className="playPause">
+            <i onClick={this.pressPlay} id="videoPlay" className="fa fa-play"></i>
+            <i onClick={this.pressPlay} id="videoPause" className="fa fa-pause hidePlay"></i>
+          </div>
+          <input onChange={this.handleSlide} type="range" id="seeker" value={this.state.seekerVal}/>
         </div>
       </div>
     );
