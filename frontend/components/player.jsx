d89a6281f70758c225dc5fccad1f6a6f36359b96
pauliewax <pauliewax@gmail.com>
2017-04-27 11:35:40 -0400

    Autoplay on

--- frontend/components/player.jsx
+++ frontend/components/player.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchVideo } from '../actions/video_actions';
 
 class Player extends React.Component {
   constructor(props) {
@@ -7,24 +8,37 @@ class Player extends React.Component {
   }
 
   componentWillMount() {
-    
+    this.props.fetchVideo(this.props.params.id);
   }
 
   render() {
-    // <video></video>
-    return(
-      <div>SUP YOU MADE IT</div>
-    );
+
+    if (this.props.video) {
+      return(
+        <div className="player">
+          <video id="videoPlayer" controls autoPlay>
+            <source src={this.props.video.video_url} type="video/mp4"></source>
+            Your browser does not video playback.
+          </video>
+        </div>
+      );
+    } else {
+      return (
+        <div></div>
+      );
+    }
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
   return {
+    video: state.videos.videoDetail
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
+    fetchVideo: (id) => (dispatch(fetchVideo(id)))
   };
 };
 
