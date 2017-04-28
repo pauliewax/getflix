ff41044225664f5c961473014d92ce2dd5d68c8c
pauliewax <pauliewax@gmail.com>
2017-04-28 00:36:40 -0400

    Broken favs list renders on browse

--- app/views/api/follows/index.json.jbuilder
+++ app/views/api/follows/index.json.jbuilder
@@ -2,7 +2,7 @@ json.array! @follows do |follow|
     json.id follow.series.id
     json.title follow.series.title
     json.description follow.series.description
-    json.url follow.series.thumbnail_url
+    json.thumbnail_url follow.series.thumbnail_url
     json.year follow.series.year
     json.rating follow.series.mpaa_rating
     json.firstEp follow.series.videos.where('episode_num = 1 ')
--- frontend/components/browse.jsx
+++ frontend/components/browse.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchFollows } from '../actions/follow_actions';
+import SeriesRow from './series_row';
 
 class Browse extends React.Component {
  constructor(props) {
    super(props);
  }
 
+ componentWillMount() {
+   this.props.fetchFollows();
+ }
+
+ generateRows() {
+   let allSeries = Object.assign([], this.props.follows);
+   let allRows =[];
+   while (allSeries.length !== 0)  {
+     allRows.push(allSeries.slice(0,6));
+     allSeries = allSeries.slice(6);
+   }
+   return allRows;
+ }
+
  render()  {
+   let seriesRows= "";
+
+   if (this.props.follows) {
+     let rows = this.generateRows();
+     seriesRows = rows.map(row=><SeriesRow key={row[0].id} row={row} rowId={row[0].id} />);
+   }
+
    return(
      <div className="browser" >
        <section className="browse-splash">
@@ -16,10 +39,23 @@ class Browse extends React.Component {
            <img className="splash-logo" src="http://www.pauliewax.com/wp-content/uploads/2017/04/getflixoriginal.png" />
            <text>Dry air. Wet code. Escape is not defined. See how it all unfolds when the stack limit is reached in this critically acclaimed spaghetti coder.</text>
          </aside>
+         { seriesRows }
        </section>
      </div>
    );
  }
 }
 
-export default Browse;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    follows: state.follows
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    fetchFollows: () => (dispatch(fetchFollows()))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Browse);
