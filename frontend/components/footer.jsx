0550f0a442351adf207a9b63f03b77488b8a7a90
pauliewax <pauliewax@gmail.com>
2017-06-08 15:51:50 -0400

    Create footer links

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -14,13 +14,21 @@ class Footer extends React.Component {
         <main>
           <aside>Questions? Email paul.jo.eng@gmail.com</aside>
           <ul>
-            <li>LinkedIn</li>
-            <li>GitHub</li>
-            <li>Portfolio</li>
-            <li>Resume</li>
-            <li>AngelList</li>
-            <li>Email</li>
+            <li><a href='https://www.linkedin.com/in/pauljoeng/' target='_blank'>LinkedIn</a></li>
+            <li><a href='https://github.com/pauliewax/' target='_blank'>GitHub</a></li>
+            <li><a href='http://paul-eng.com/' target='_blank'>Portfolio</a></li>
+            <li><a href='http://paul-eng.com/assets/resume.pdf' target='_blank'>Resume</a></li>
+            <li><a href='https://angel.co/paul-eng' target='_blank'>AngelList</a></li>
+            <li><a href='mailto:paul.jo.eng@gmail.com'>Email</a></li>
           </ul>
+          <div className ="iconPanel">
+            <i className="fa fa-linkedin"></i>
+            <i className="fa fa-angellist"></i>
+            <i className="fa fa-github"></i>
+            <i className="fa fa-drivers-license"></i>
+            <i className="fa fa-envelope"></i>
+            <i className="fa fa-file"></i>
+          </div>
           <p>© 1989-2017 Paul Eng, Inc.</p>
         </main>
       </div>
